0cb8b4e047262db9d19abaa29f9ae72c0a43b20a
Gustavo <gustavoemersonfd@gmail.com>
2022-09-06 15:18:25 -0300

    ApiKey

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,19 +8,12 @@ import java.net.http.HttpResponse;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //Url da Api
         var apiURL = "https://imdb-api.com/en/API/Top250TVs/";
-        //Api Key
-        var apiKey = "k_0nwp78p7";
-        //Um Identificador Uniforme de Recursos é uma sequência de caracteres usada para a identificação de um recurso específico.
+        var apiKey = "apiKey";
         var apiIMDB = URI.create(apiURL + apiKey);
-        //se comporta como um contêiner para informações de configuração comuns a várias solicitações
         var client = HttpClient.newHttpClient();
-        //representa a solicitação a ser enviada por meio do HttpClient
         var request = HttpRequest.newBuilder().uri(apiIMDB).build();
-        //representa o resultado de uma chamada HttpRequest
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        //corpo da requisição
         var json = response.body();
         System.out.println("Response: " + json);
     }
